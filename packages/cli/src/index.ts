6c4ce44cfbe1f52bb7858ccfbb8690ce5566c2d6
Nicolas Schwandner <nico.schwandner@gmail.com>
2025-05-06 17:57:33 +0200

    feat: Implement work-journal new command (Closes #4)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,10 +2,12 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers"; // Import hideBin
 import { initCommand } from "./commands/init"; // Import the init command module
+import { newCommand } from "./commands/new"; // Import the new command module
 
 yargs(hideBin(process.argv)) // Use hideBin here
   .scriptName("work-journal")
   .command(initCommand) // Register the init command module
+  .command(newCommand) // Register the new command module
   .demandCommand(1, "Please specify a command.") // Update demandCommand message
   .strict() // Enable strict mode
   .help()
